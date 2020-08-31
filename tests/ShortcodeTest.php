857f53ba5cf4cc120f134bf539d1817ae070fb92
ethanhinson <ethan@bluetent.com>
2020-08-30 23:57:18 -0600

    Move around how we bootstrap WP, test lodging item shortcode

---
+++ tests/ShortcodeTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ethan
+ * Date: 8/30/20
+ * Time: 11:04 PM
+ */
+
+namespace Rezfusion\Tests;
+
+
+class ShortcodeTest extends BaseTestCase {
+
+  /**
+   * Test that the shortcode renders as expected. Taking into account when a
+   * developer overrides the template in their active theme.
+   */
+  public function testLodgingItem() {
+    rezfusion_components_update_item_data();
+    $posts = get_posts(['post_type' => 'vr_listing']);
+    $meta = get_post_meta($posts[0]->ID);
+    $out = do_shortcode("[rezfusion-lodging-item itemid=\"{$meta['rezfusion_hub_item_id'][0]}\"]");
+    $doc = new \DOMDocument();
+    $doc->loadXML($out);
+    $xpath = new \DOMXPath($doc);
+    $wrappers = $xpath->query('//div[contains(@class,"lodging-item")]');
+    $this->assertEquals(5, $wrappers->length);
+    $beds = $xpath->query('//div[contains(@class, "lodging-item-details__beds")]');
+    $this->assertEquals(1, $beds->length);
+    $baths = $xpath->query('//div[contains(@class, "lodging-item-details__baths")]');
+    $this->assertEquals(1, $baths->length);
+
+    // Copy the template file to the active theme.
+    $activeTheme = get_stylesheet_directory();
+    copy(__DIR__ . '/../templates/vr-details-page.php', $activeTheme . '/vr-details-page.php');
+    file_put_contents($activeTheme . '/vr-details-page.php', file_get_contents($activeTheme . '/vr-details-page.php') . "\nTHEME TEMPLATE");
+    $out2 = do_shortcode("[rezfusion-lodging-item itemid=\"{$meta['rezfusion_hub_item_id'][0]}\"]");
+    $this->assertTrue(!!stristr($out2, 'THEME TEMPLATE'));
+  }
+
+  public function tearDown(): void {
+    parent::tearDown();
+    $activeTheme = get_stylesheet_directory();
+    unlink($activeTheme . '/vr-details-page.php');
+  }
+
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,4 @@
 <?php
 require_once(__DIR__ . "/../vendor/autoload.php");
 
-// Bootstrap wordpress.
-require_once(__DIR__ . "/../../../../wp-load.php");
-require_once(__DIR__ . "/../../../../wp-settings.php");
+
