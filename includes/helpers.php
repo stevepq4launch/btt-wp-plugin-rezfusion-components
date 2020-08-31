fb9506e047f6639f9679cc4dd03a22646f60eb6f
ethanhinson <ethan@bluetent.com>
2020-08-31 01:09:14 -0600

    Remove dead code

--- includes/helpers.php
+++ includes/helpers.php
@@ -4,23 +4,6 @@
  */
 
 /**
- * Insert a value or key/value pair after a specific key in an array.  If key doesn't exist, value is appended
- * to the end of the array.
- *
- * @param array $array
- * @param string $key
- * @param array $new
- *
- * @return array
- */
-function _array_insert_after( array $array, $key, array $new ) {
-  $keys = array_keys( $array );
-  $index = array_search( $key, $keys );
-  $pos = false === $index ? count( $array ) : $index + 1;
-  return array_merge( array_slice( $array, 0, $pos ), $new, array_slice( $array, $pos ) );
-}
-
-/**
  * Provide a helper function to describe the environement.
  *
  * @return string
