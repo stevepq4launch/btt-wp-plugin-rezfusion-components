823f6b8444c5ab1cdd6fe9b9197b384524b95235
Stephen Parker <stephen.parker@q4launch.com>
2021-06-17 13:13:13 -0400

    Fix property image gallery label text
    Replace "Foo" placeholder in property gallery image labels with unit
    name.

--- templates/vr-item-photos.php
+++ templates/vr-item-photos.php
@@ -4,5 +4,5 @@
  * @var $lodgingItem
  */
 ?>
-<div data-rezfusion-photos="<?php print esc_js(json_encode($lodgingItem->item->images)); ?>" class="lodging-item-details__photos">
+<div data-rezfusion-photos="<?php print esc_js(json_encode($lodgingItem->item->images)); ?>" data-rezfusion-item-name="<?php print esc_js(json_encode($lodgingItem->item->name)) ?>" class="lodging-item-details__photos">
 </div>
