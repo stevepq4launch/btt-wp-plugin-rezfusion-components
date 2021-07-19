db6c3d87e579fcb96c82f30b408cca77d26ba782
ppokojowczyk <piotr.pokojowczyk@q4launch.com>
2021-07-19 16:04:05 +0200

    fix: data-rezfusion-photos JSON string escaping characters issue.

--- templates/vr-favorite-toggle.php
+++ templates/vr-favorite-toggle.php
@@ -6,4 +6,4 @@
  */
 ?>
 
-<div class="favorite-toggle" data-rezfusion-item-id="<?php print esc_js(json_encode($lodgingItem->item->id)) ?>" data-rezfusion-item-name="<?php print esc_js(json_encode($lodgingItem->item->name)) ?>" data-rezfusion-toggle-type="<?php print esc_js(json_encode(($type === 'small' || $type === 'large') ? $type : 'small')) ?>"></div>
+<div class="favorite-toggle" data-rezfusion-item-id="<?php print esc_js(json_encode($lodgingItem->item->id)) ?>" data-rezfusion-item-name="<?php print stripslashes(esc_js(json_encode($lodgingItem->item->name))) ?>" data-rezfusion-toggle-type="<?php print esc_js(json_encode(($type === 'small' || $type === 'large') ? $type : 'small')) ?>"></div>
