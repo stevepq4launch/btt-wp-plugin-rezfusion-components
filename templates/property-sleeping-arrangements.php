b8bce28d9217f75e22160e5140534cb770db6488
Alex Nordhausen <anorthhouse@gmail.com>
2021-09-10 06:36:10 -0600

    chore: reset to prev commit

--- templates/property-sleeping-arrangements.php
+++ templates/property-sleeping-arrangements.php
@@ -1,4 +1,5 @@
 <?php
 $roomData = json_encode($lodgingItem->item->rooms);
 ?>
+
 <div class="sleeping-arrangements" data-rezfusion-rooms="<?php print esc_js($roomData); ?>"></div>
--- templates/vr-sleeping-arrangements.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @var string $rooms room data
- */
-?>
-
-<div class="sleeping-arrangements" data-rezfusion-rooms="<?php print esc_js($rooms); ?>"></div>
