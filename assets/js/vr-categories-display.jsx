8c0f2340d2c1b33355828b1560ca12d066ec5720
Alex Nordhausen <anorthhouse@gmail.com>
2021-11-16 05:43:32 -0700

    chore: cleanup

--- assets/js/vr-categories-display.jsx
+++ assets/js/vr-categories-display.jsx
@@ -5,13 +5,10 @@ const el = document.querySelector('#lodging-item-details_categories-display');
 if (el) {
   (async function () {
     const { ThemeProvider } = await import('styled-components');
-    const { defaultTheme } = await import('@propertybrands/btt-bluetent-components/lib/styles');
     const { getConfigOption } = await import('@propertybrands/btt-bluetent-components/lib/opts');
     const { default: ConfigProvider } = await import('@propertybrands/btt-bluetent-components/components/ConfigContext/ConfigProvider');
     const { defaultTheme } = await import('@propertybrands/btt-bluetent-components/lib/styles');
-    const { getConfigOption } = await import('@propertybrands/btt-bluetent-components/lib/opts');
-    const images = JSON.parse(el.dataset.rezfusionPhotos.toString());
-    const itemName = JSON.parse(el.dataset.rezfusionItemName.toString());
+
     ReactDOM.render(
       <ConfigProvider>
         <ThemeProvider>
