16190c7f141fa47efed5cf772f0bd9f69d27f29e
Alex Nordhausen <anorthhouse@gmail.com>
2021-11-16 06:23:56 -0700

    wip

--- assets/js/vr-categories-display.jsx
+++ assets/js/vr-categories-display.jsx
@@ -5,18 +5,37 @@ const el = document.querySelector('#lodging-item-details_categories-display');
 if (el) {
   (async function () {
     const { ThemeProvider } = await import('styled-components');
-    const { getConfigOption } = await import('@propertybrands/btt-bluetent-components/lib/opts');
     const { default: ConfigProvider } = await import('@propertybrands/btt-bluetent-components/components/ConfigContext/ConfigProvider');
+    const { default: DetailsProvider } = await import('@propertybrands/btt-bluetent-components/components/DetailsContext/DetailsProvider');
     const { defaultTheme } = await import('@propertybrands/btt-bluetent-components/lib/styles');
 
-    ReactDOM.render(
-      <ConfigProvider>
-        <ThemeProvider>
-          <DetailsProvider>
+    const itemId = 'SXRlbTo5Nzc1';
+    const config = {
+      settings: {
+        components: {
+          SearchProvider: {
+            channels: 'https://easternshorevacations.com',
+            endpoint: 'http://host.docker.internal:3000/graphql',
+          },
+          AvailabilitySearchConsumer: {
+            spsDomain: 'https://checkout.rezfusion.com',
+            confirmationPageL: '',
+          },
+          Checkout: {
+            options: {},
+          },
+        },
+      },
+    };
+
+    ReactDOM.render((
+      <ConfigProvider userProvided={config}>
+        <ThemeProvider theme={{ ...defaultTheme }}>
+          <DetailsProvider itemId={itemId}>
             <h1>Hello world!</h1>
           </DetailsProvider>
         </ThemeProvider>
-      </ConfigProvider>, el
-    );
+      </ConfigProvider>
+    ), el);
   }());
 }
---
+++ templates/vr-categories-display.php
@@ -0,0 +1,1 @@
+<div id="lodging-item-details_categories-display"></div>
